f2dad203a8563cfd33991d061aeda6d6718f21be
2much2write <hafidhaw@gmail.com>
2024-03-25 10:27:58 +0700

    Exercise 1 Test

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,21 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise1 {
-}
+    public static void main(String[] args) {
+        int[] array = {7, 2, 5, 1, 4};
+        int max = findLargest(array);
+        System.out.println("The largest element in the array is: " + max);
+    }
+        public static int findLargest ( int[] array){
+            int max = array[0];
+
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+    }
+
